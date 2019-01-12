2bddc6e223e0cf7602934a0b5151de4d0b3622ab
majesticrock <joshua.althueser@web.de>
2019-01-12 14:11:53 +0100

    Fehlen nur noch Referenzen

--- plot-phasenverschiebung.py
+++ plot-phasenverschiebung.py
@@ -16,10 +16,19 @@ werte = csv_read("csv/variable-frequenz.csv")
 xdata = np.zeros(26)
 ydata = np.zeros(26)
 
+fx = np.zeros(2)
+fy = np.zeros(2)
+
 ignore = 3
 i=0
+j=0
 for values in werte:
     if(ignore >= 1):
+        if(ignore <=2):
+            fx[j] = float(values[0]) * 2 * np.pi
+            fy[j] = float(values[2])*(float(values[0]) * 2 * np.pi) * 10**(-6)
+            j+=1
+
         ignore = ignore - 1
     else:
         xdata[i] = float(values[0]) * 2 * np.pi
@@ -28,7 +37,9 @@ for values in werte:
 
 x_line = np.append([np.linspace(10, 100), np.linspace(100, 1000)], np.linspace(1000, 10000)) * 2 * np.pi
 
+
 plt.plot(xdata, ydata, "r.", label="Messwerte")
+plt.plot(fx, fy, "ro", label="Nicht betrachtete Messwerte")
 popt, pcov = curve_fit(func, xdata, ydata)
 plt.plot(x_line, func(x_line, *popt), "b-", label="Fit")
 
--- polarplot.py
+++ polarplot.py
@@ -15,10 +15,18 @@ def func(x, a):
 werte = csv_read("csv/variable-frequenz.csv")
 phi = np.zeros(26)
 rdata = np.zeros(26)
+fphi = np.zeros(2)
+fr = np.zeros(2)
 ignore = 3
 i=0
+j=0
 for values in werte:
     if(ignore >= 1):
+        if(ignore <=2):
+            fphi[j] = float(values[2])*float(values[0]) * (2*np.pi) * 10**(-6)
+            fr[j] = (float(values[1])/15)
+            j+=1
+        
         ignore = ignore - 1
     else:
         phi[i] = float(values[2])*float(values[0]) * (2*np.pi) * 10**(-6)
@@ -28,7 +36,8 @@ for values in werte:
 phi_line = np.linspace(0,np.pi/2)
 
 plt.polar(phi, rdata, "r.", label="Messwerte")
-plt.polar(phi_line, np.cos(phi_line), "b-", label="Fit")
+plt.polar(fphi, fr, "ro", label="Nicht betrachete Messwerte")
+plt.polar(phi_line, np.cos(phi_line), "b-", label="Theoriekurve")
 
 plt.legend()
 plt.tight_layout()
